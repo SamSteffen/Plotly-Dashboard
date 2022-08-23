d9c62b626791f14c7f774dfc6bab29cbce0c87d1
SamSteffen <atticus.steffen@gmail.com>
2022-08-22 22:12:05 -0600

    Added more code.

--- plots.js
+++ plots.js
@@ -1,41 +1,42 @@
 function init() {
-    var selector = d3.select("#selDataset");
-  
-    d3.json("samples.json").then((data) => {
-      console.log(data);
-      var sampleNames = data.names;
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  })};
-  
-  init();
+  var selector = d3.select("#selDataset");
 
-  function optionChanged(newSample) {
-    // console.log(newSample);
-    buildMetadata(newSample);
-    buildCharts(newSample);  
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})};
+
+init();
+
+function optionChanged(newSample) {
+  // console.log(newSample);
+  buildMetadata(newSample);
+  //buildCharts(newSample);  
 }
 
 function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray;
-      var PANEL = d3.select("#sample-metadata");
-  
-      PANEL.html("");
-      PANEL.append("h6").text("ID: " + result.id);
-      PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
-      PANEL.append("h6").text("Gender: " + result.gender);
-      PANEL.append("h6").text("Age: " + result.age);
-      PANEL.append("h6").text("Location: " + result.location);
-      PANEL.append("h6").text("BB-type: " + result.bb_type);
-      PANEL.append("h6").text("WFREQ: " + result.wfreq);
-    });
-  }
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
+    PANEL.append("h6").text("GENDER: " + result.gender);
+    PANEL.append("h6").text("AGE: " + result.age);
+    PANEL.append("h6").text("LOCATION: " + result.location);
+    PANEL.append("h6").text("BB-TYPE: " + result.bbtype);
+    PANEL.append("h6").text("WFREQ: " + result.wfreq);
+  });
+}
+
 
   
