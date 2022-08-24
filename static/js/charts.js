a273eb9431970312c15f5ebb854ed7c0d58bcd12
SamSteffen <atticus.steffen@gmail.com>
2022-08-23 22:36:53 -0600

    Added code.

--- static/js/charts.js
+++ static/js/charts.js
@@ -60,7 +60,7 @@ function buildCharts(sample) {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var filteredSamples = samples.filter(sampleObj => sampleObj.id == sample);
+    var filteredSamples = samplesArray.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
     firstSample = filteredSamples[0];
 
