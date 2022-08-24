d2a3f5749c0b5ca08eaafcae317c6250ed3b0c6e
SamSteffen <atticus.steffen@gmail.com>
2022-08-23 22:02:13 -0600

    Added updated code.

--- static/js/charts.js
+++ static/js/charts.js
@@ -58,30 +58,38 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samplesArray = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var filteredSamples = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
+    firstSample = filteredSamples[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIds = firstSample.otu_ids;
+    var otuLabels = firstSample.otu_labels;
+    var sampleValues = firstSample.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [
-      
-    ];
+      {
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks,
+      hovertext: otu_labels.slice(0, 10).reverse(),
+      type: 'bar'
+    }];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top Bellybutton Bacteria"
     };
-    // 10. Use Plotly to plot the data with the layout. 
     
+    // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar-area", barData, barLayout)
   });
 }
