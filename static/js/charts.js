8461119eb3b79c971fed2c8ecdadf8035485140b
SamSteffen <atticus.steffen@gmail.com>
2022-08-24 23:58:01 -0600

    Adding modified code.

--- static/js/charts.js
+++ static/js/charts.js
@@ -144,15 +144,15 @@ function buildCharts(sample) {
       type: 'indicator',
       mode: 'gauge+number',
       gauge: {
-        axis: {range: [0, 10]},
+        axis: {range: [null, 10]},
         bar: {color: 'black'},
         steps: [
           { range: [0,2], color: 'red' },
           { range: [2,4], color: 'orange' },
           { range: [4,6], color: 'yellow' },
           { range: [6,8], color: 'lightgreen' },
-          { range: [8,10], color: 'green' },
-          ],
+          { range: [8,10], color: 'green' }
+          ]
       }
     }];
       
